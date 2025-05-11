9693ff441c6f2065256856b65ae13ea74e563d6d
LuciaCembellin <20110702@mail.wit.ie>
2025-05-11 14:48:02 +0100

    Player Movement and Animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public float mouseSensitivity = 25f;
+
+    private Vector3 moveDirection;
+    private Animator anim;
+    private float yRotation = 0f;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        // Get the initial rotation for the player
+        yRotation = transform.eulerAngles.y;
+
+        // Block and disable mouse pointer
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        RotateWithMouse();
+        Move(moveDirection);
+    }
+
+    void RotateWithMouse()
+    {
+        float mouseX = Mouse.current.delta.x.ReadValue();
+        yRotation += mouseX * mouseSensitivity * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
+    }
+
+    public void OnMove(InputValue input)
+    {
+        Vector2 inputVec = input.Get<Vector2>();
+        moveDirection = new Vector3(inputVec.x, 0, inputVec.y);
+
+        // Update Blend tree
+        anim.SetFloat("moveX", inputVec.x);
+        anim.SetFloat("moveY", inputVec.y);
+    }
+
+    void Move(Vector3 direction)
+    {
+        Vector3 move = transform.TransformDirection(direction);
+        transform.position += move * moveSpeed * Time.deltaTime;
+    }
+}
